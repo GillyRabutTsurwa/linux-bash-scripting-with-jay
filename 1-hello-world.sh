44deb23f2db44a686c00c0eff489ad87129d96ab
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-08-30 16:47:17 -0400

    First Commit / Bash Scripting Settings

---
+++ 1-hello-world.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# the line above indicates what interpretor to use (bash, zsh, etc)
+# the line above tells the following script to use the Bash interpretor
+# apparently, I can use this whilst using any interpretor
+# so, i can be using zsh, but with the line above, the script will execute using bash
+# nice. very nice
+# last thing, the first line is NOT a comment. event though it's starts with a "#"
+
+echo "Hello World. Bienvenue à Bash Scripting"
+echo "Je peux utiliser Nano ou VSCode. C'est top"
+echo "Mon directory actuel c'est: "
+pwd
+echo "On y va. Allez"
