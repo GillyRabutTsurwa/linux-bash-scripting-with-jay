eebd48969250ad1bdea258c4640804563ebea5ff
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-12-22 15:11:51 -0500

    Placement of Exit Code Statements Matters - this is the takeaway of this commit and the last commit - read the notes on this commit file as well

--- exit-codes.sh
+++ exit-codes.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-directory=/nonexistent
+directory=/etc
 
 if [ -d $directory ]
 then
+	echo $?
 	echo "The directory $directory exists."
 else
+	echo $?
 	echo "The direcotry $directory does not exist."
 fi
 
-echo "The exit code for this script run is $?"
-
-# IMPORTANT
-# notice how even if i simulate an error, like i am doing when i change the directory value to /nonexistent...
-# ... i still get an exit code of 0
-# this is because the value of the exit code is actually based on the echo statements
-# meaning, if the echo statements will run ― even the one that is supposed to when the if statement results to false ―
-# then the exit code will be 0
-# the takeaway here is the placement of your exit code lines is very important
+# i altered the script so that the exit code value appropriately reflects our condition status
+# notice how i put the exit code before and not after the echo that prints if the directory exists or not
+# this is because if i put it after, the exit code will always be 0, because it will based on that echo statement, which we don't want
+# move that line from above that echo statement to below it to test it
+# again, just like in the last commit, the takeaway is where you put your exit code lines are very important
