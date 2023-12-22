13c5ca4bd3290598a2ac3797d84e862954828a7b
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-12-22 16:21:35 -0500

    While Loops Example #3

--- while-loops.sh
+++ while-loops.sh
@@ -2,17 +2,11 @@
 
 while [ -f ~/Bureau/fichier.txt ]
 do
-	echo "The file exists"
+	echo "Depuis le $(date), le fichier existe"
 	sleep 5s
 done
 
-echo "The file no longer exists. Exiting"
+echo "Depuis le $(date), le fichier est manquant"
 
-# Attention
-# as long as the file in question exists, the echo statement in the while loop will continue to be printed
-# this does not cause an infinite loop persé, but it surely behaves like one
-# obviously when we remove the file, the loop stops
-# do not see how this is relevant à l'instant
-# the fans kicked in not too long after script execution, as expected
-# to make things less overwhelming for the cpu and more readable, i will add the sleep command
-# this will execute the code inside the while loop after every 5 seconds in my case
+# here we are using the date command as a subshell to print out date info
+# as you can see, the date updates along with the progression of the loop
