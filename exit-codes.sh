da3731af19e285eb3b3f5158421d8e5977307c0c
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-12-22 14:56:00 -0500

    Exit Codes (Bad Example / Important Lesson) - consult the notes in this commit that are found within the file itself

--- exit-codes.sh
+++ exit-codes.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-package=nonexistent
+directory=/nonexistent
 
-sudo apt install $package >> command-output.log
-
-if [ $? -eq 0 ]
-then 
-	echo "The installation of the package was successful"
-	echo "The new command is available here:"
-	which $package
+if [ -d $directory ]
+then
+	echo "The directory $directory exists."
 else
-	echo "The package failed to install" >> command-failure.log
+	echo "The direcotry $directory does not exist."
 fi
+
+echo "The exit code for this script run is $?"
+
+# IMPORTANT
+# notice how even if i simulate an error, like i am doing when i change the directory value to /nonexistent...
+# ... i still get an exit code of 0
+# this is because the value of the exit code is actually based on the echo statements
+# meaning, if the echo statements will run ― even the one that is supposed to when the if statement results to false ―
+# then the exit code will be 0
+# the takeaway here is the placement of your exit code lines is very important
