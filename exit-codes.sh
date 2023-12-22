31584bb74d56a5e02e002de989e79c6177738da4
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-12-22 15:22:42 -0500

    Enforcing Exit Codes & The Nature of Exit Codes - i have some very important notes in this commit file - to summarise: you can enforce exit code values - and also, exit codes stop script processing at that point, regardless of scope - is scope even a thing in bash? anyways, we move

--- exit-codes.sh
+++ exit-codes.sh
@@ -4,15 +4,25 @@ directory=/etc
 
 if [ -d $directory ]
 then
-	echo $?
 	echo "The directory $directory exists."
+	exit 0
+	echo "No, putting an echo statement here won't work either"
 else
-	echo $?
 	echo "The direcotry $directory does not exist."
+	exit 1
+	echo "Neither will it work here"
 fi
 
-# i altered the script so that the exit code value appropriately reflects our condition status
-# notice how i put the exit code before and not after the echo that prints if the directory exists or not
-# this is because if i put it after, the exit code will always be 0, because it will based on that echo statement, which we don't want
-# move that line from above that echo statement to below it to test it
-# again, just like in the last commit, the takeaway is where you put your exit code lines are very important
+echo "You won't see this statement upon script execution"
+echo "Celui-ci non plus"
+echo "Neither will you see this one"
+
+# IMPORANT
+# we can use the exit keyword followed by a value to force the exit code value of the previous command, as i am doing here
+# pretty much enforcing that value, regardless of the nature of that command
+# that is lesson #1
+# lesson #2 is that after an exit code, no other lines of code after that will be run
+# the behaviour of the exit keyword is rather similar to that of the return keyword in javascript
+# anything after the return keyword in javascript is inexecutable. same applies here
+# the difference i do see however is that even the 3 last lines won't run, where as in javascript, they would run due to scope
+# exit pretty much stops the processing of that file at that point; anything after that will be irrelevant
