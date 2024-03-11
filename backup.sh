e0fc6657f8161edbbb96d5abbb2fe8c5a2f4a036
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2024-03-11 14:22:26 -0400

    Making a Backup Script: #2 (Meilleur) - version 1 of the backup script - i will remove comments on the subsequent versions - this script will do two things 	- recursively backup a directory and all its contents 	- overwrites existing backed files whilst "moving" the original to a date-labeled backup directory 	- if this preceding point doesn't make sense, read the comments within this file - read comments altogether, ils vont tout expliquer

--- backup.sh
+++ backup.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
-# un peu de nettoyage avant d'executer le script
-# ca me servira bien pour le moment
-rm -rf $1/backup;
-rm $1/*.log;
-
-if [ $# -ne 3 ]
+if [ $# -ne 2 ]
 then
-	echo "Usage: backup.sh <parent_directory> <source_directory> <target_directory>";
-	echo "This script needs exactly 3 arguements";
-	echo "Try again";
+	echo "Usage: backup.sh <source_directory> <target_directory>";
+	echo "This script needs exactly 2 arguements";
 	exit 1;
-fi
-
-if [ ! -d "backup" ]
-then
-	mkdir $1/backup;
 fi
 
 if !command -v rsync &> /dev/null
@@ -26,6 +15,7 @@ then
 fi
 
 current_date=$(date +%d-%m-%Y);
+
 # --archive (-a) = archive mode
 # --verbose (-v) = verbose
 # --backup (-b) = does not replace files in the target file if file already exists in target
@@ -33,10 +23,23 @@ current_date=$(date +%d-%m-%Y);
 # --delete = makes sure target directory is a clone of the source directory. use with caution. deleting a folder in the source directory and re-running rsync will delete in the target directory
 # but with -b and --backup-dir options, nothing will actually be deleted. they will instead be moved to the backup directory
 # --dry-run: IMPORTANT: this will perform a test run of your command and not actually execute anything. this is paramount in testing your scripts
-rsync_options="--archive --verbose --backup --backup-dir $2/current_date --delete"; # le meme code que celui dessus sans l'option de tester (--dry-run). les options ont ete aussi eloignees
 
-$(which rsync) $rsync_options $2 $3/current >> backup_$current_date.log;
+# ici, le deuxieme argument du script sera le nom du dossier de destination ou nos fichiers and dossiers a partir de la source seront copies
+if [ ! -d $2 ]
+then
+	mkdir $2 --parents; # mkdir -p $2 (for Mac)
+fi
 
+rsync_options="--archive --verbose --backup --backup-dir=$2/$current_date --delete"; # le meme code que celui dessus sans l'option de tester (--dry-run). les options ont ete aussi eloignees
+# --archive (-a) = archive mode
+# --verbose (-v) = verbose
+# --backup (-b) = does not replace files in the target file if file already exists in target
+# --backup-dir = directory location for the backup. this flag is used in conjunction with the --backup (-b) flag
+# --delete = makes sure target directory is a clone of the source directory. use with caution. deleting a folder in the source directory and re-running rsync will delete in the target directory
+# but with -b and --backup-dir options, nothing will actually be deleted. they will instead be moved to the backup directory
+# --dry-run: IMPORTANT: this will perform a test run of your command and not actually execute anything. this is paramount in testing your scripts
+
+$(which rsync) $rsync_options $1 $2/current >> backup_$current_date.log;
 # Attention:
 # ce code-ci:
 # ./backup.sh text-files backup
